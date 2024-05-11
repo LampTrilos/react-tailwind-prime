63cc52a0765a6eee32b5f00b41bdf13c0e8d1d56
L.Trilos <lamprostriantafyllopoulos@yahoo.com>
2024-05-11 16:42:06 +0300

    Init

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,9 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 
+import { PrimeReactProvider } from 'primereact/api';
+import Tailwind from "primereact/passthrough/tailwind";
+
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
@@ -15,8 +18,10 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
+      <PrimeReactProvider value={{ unstyled: true, pt: {} }}>
     <html lang="en">
       <body className={inter.className}>{children}</body>
     </html>
+      </PrimeReactProvider>
   );
 }
